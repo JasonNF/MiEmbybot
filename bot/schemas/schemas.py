527cd4fd47498e6b533e2d919732212bf3df9bfc
JasonNF <31536642+JasonNF@users.noreply.github.com>
2025-09-12 14:58:30 +0800

    更新 schemas.py

--- bot/schemas/schemas.py
+++ bot/schemas/schemas.py
@@ -65,7 +65,7 @@ class Open(BaseModel):
 
 
 class Ranks(BaseModel):
-    logo: str = "SAKURA"
+    logo: str = "Misaya"
     backdrop: bool = False
 
 
@@ -112,7 +112,7 @@ class MP(BaseModel):
 
 class AutoUpdate(BaseModel):
     status: bool = True
-    git_repo: Optional[str] = "berry8838/Sakura_embyboss"  # github仓库名/魔改的请填自己的仓库
+    git_repo: Optional[str] = "JasonNF/MiEmbybot"  # github仓库名/魔改的请填自己的仓库
     commit_sha: Optional[str] = None  # 最近一次commit
     up_description: Optional[str] = None  # 更新描述
 
